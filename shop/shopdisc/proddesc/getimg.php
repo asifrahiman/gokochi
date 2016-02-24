2a5b1efb00402087cb74a30df57aab22dfff62d0
asif abdul rahiman <asif abdul rahiman>
2016-02-24 19:19:36 +0530

    image loading

--- shop/shopdisc/proddesc/getimg.php
+++ shop/shopdisc/proddesc/getimg.php
@@ -1,7 +1,7 @@
 <?php
 $id = $_GET['id'];
   
-  mysql_connect("localhost", "root", "") OR DIE (mysql_error());
+  mysql_connect("localhost", "asif","asif") OR DIE (mysql_error());
   mysql_select_db("shops");
   $sql = "SELECT shop_image FROM shops WHERE shop_id=$id";
   $result = mysql_query("$sql");
--- shop/shopdisc/proddesc/getimg1.php
+++ shop/shopdisc/proddesc/getimg1.php
@@ -1,6 +1,6 @@
 <?php
 $id = $_GET['id'];
-  mysql_connect("localhost", "root", "") OR DIE (mysql_error());
+  mysql_connect("localhost", "asif","asif") OR DIE (mysql_error());
   mysql_select_db("shops");
   $sql = "SELECT image FROM products WHERE id=$id";
   $result = mysql_query("$sql");
