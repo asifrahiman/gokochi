deac7717316a01b2bbf15a3b8b2bf647a32161df
asif abdul rahiman <asif abdul rahiman>
2016-02-24 19:52:36 +0530

    hai

--- travel/placedesc/getimg.php
+++ travel/placedesc/getimg.php
@@ -1,7 +1,7 @@
 <?php
 $id = $_GET['id'];
   
-  mysql_connect("localhost", "root", "") OR DIE (mysql_error());
+  mysql_connect("localhost", "asif", "asif") OR DIE (mysql_error());
   mysql_select_db("travel");
   $sql = "SELECT image FROM tourism WHERE key1=$id";
   $result = mysql_query("$sql");
--- travel/placedesc/getimg1.php
+++ travel/placedesc/getimg1.php
@@ -1,7 +1,7 @@
 <?php
 $id = $_GET['prodid'];
 $name=$_GET['name']; 
-  mysql_connect("localhost", "root", "") OR DIE (mysql_error());
+  mysql_connect("localhost", "asif", "asif") OR DIE (mysql_error());
   mysql_select_db("asif");
   $sql = "SELECT image FROM $name WHERE prodid=$id";
   $result = mysql_query("$sql");
