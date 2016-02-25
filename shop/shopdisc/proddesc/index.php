78bdede778cc7cf6c6ae48b24cce4fd979caf9dc
asif abdul rahiman <asif abdul rahiman>
2016-02-25 18:23:05 +0530

    asd

--- shop/shopdisc/proddesc/index.php
+++ shop/shopdisc/proddesc/index.php
@@ -17,7 +17,6 @@
 
     <!-- Custom CSS -->
     <link href="css/shop-item.css" rel="stylesheet">
-	<link href="css/simple-sidebar.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -75,53 +74,44 @@
         </div>
         <!-- /.container -->
     </nav>
-	<?php
+	<?php error_reporting(0);
 $id='1';
 if($_GET['id']!=null)
 $id = $_GET['id'];
 $name= $_GET['name'];
 				
-				
+
+				$dbname = "shops";
+				$conn = new mysqli("localhost", "asif","asif", $dbname);
 				// Check connection
 				if ($conn->connect_error) {
 					die("Connection failed: " . $conn->connect_error);
 				}
 
 				
-				?>
-					
-	<div id="wrapper">
-
-        <!-- Sidebar -->
-        <div id="sidebar-wrapper">
-            <ul class="sidebar-nav">
-                <li class="sidebar-brand">
-                    <a>Categories
-                    </a>
-                </li>
-                <li>
-                    <a  href="../index.php?id=<?php echo $row["shop_id"]?>" >all</a>
-                </li>
-                <li>
-                    <?php $sql2 = "SELECT DISTINCT `category` FROM `products` where shop_id=$id";
-					$dbname = "shops";
-					$conn = new mysqli("localhost", "asif","asif", $dbname);
-					if ($conn->query($sql2)) {
-					$result2 = $conn->query($sql2);
-					if ($result2->num_rows > 0) {
-					while($row2 = $result2->fetch_assoc()) {?>
-                    <a href="../index.php?id=<?php echo $row["shop_id"]?>&category=<?php echo $row2["category"]?>"><?php echo $row2["category"]?></a>
-					<?php }}}?>
-                </li>
+				$sql = "SELECT * FROM `asif` where id=$id";
 				
-                
-            </ul>
-        </div>
-        <!-- /#sidebar-wrapper -->
+				
+				if ($conn->query($sql)) {
 
-        <!-- Page Content -->
-        <div id="page-content-wrapper">
-            <div class="container-fluid">				
+					$result = $conn->query($sql);
+
+				if ($result->num_rows > 0) {
+					// output data of each row
+					while($row = $result->fetch_assoc()) {{ $shopname=$row["username"]; ?>
+					<?php	
+				
+				
+				$sql1 = "SELECT * FROM $shopname where name='$name' ";
+				
+				
+				if ($conn->query($sql1)) {
+
+					$result1= $conn->query($sql1);
+
+				if ($result1->num_rows > 0) {?>
+					
+					
 					
 	
 								<form action="../index.php?" method="get" novalidate>
@@ -137,296 +127,187 @@ $name= $_GET['name'];
 								</div>
 								<div id="success"></div>
 								</form>
-   
-						<div class="row">
-						<div class="col-xs-12 " >
-						<a href="#menu-toggle" class="btn btn-success" id="menu-toggle">Toggle Categories</a><br>
-						</div>
-						</div>
+    <!-- Page Content -->
+    <div class="container">
 
-				<div class="row">
+        <div class="row">
 
+            <div class="col-md-3">
+                <p class="lead"><?php echo $row["username"]?></p>
+                <div class="list-group">
+                    <a  href="http://127.0.0.1/asif/shop/shopdisc/index.php?name=<?php echo $name; ?>&id=<?php echo $row["id"]?>" class="list-group-item">all</a>
+					<?php $sql2 = "SELECT DISTINCT `category` FROM `$shopname` ";
+					if ($conn->query($sql2)) {
+					$result2 = $conn->query($sql2);
+					if ($result2->num_rows > 0) {
+					while($row2 = $result2->fetch_assoc()) {?>
+                    <a href="http://127.0.0.1/asif/shop/shopdisc/index.php?name=<?php echo $name;?>&id=<?php echo $row["id"]?>&category=<?php echo $row2["category"]?>" class="list-group-item"><?php echo $row2["category"]?></a>
+					<?php }}}?>
+                </div>
+            </div>
+
+            <div class="col-md-9">
+				<?php while($row1 = $result1->fetch_assoc()) {{?>
+                <div class="thumbnail">
+                    <img class="img-responsive" src="getimg1.php?prodid=<?php echo $row1["prodid"]?>&name=<?php echo $row["username"]?>" alt="">
+                    <div class="caption-full">
+                        <h4 class="pull-right"><i class="fa fa-fw fa-inr"></i><?php echo $row1["price"]?></h4>
+                        <h4><?php echo $row1["name"]?>
+                        </h4>
+						<h4><?php if(!$row1["stock"]){echo "out of stock";} ?></h4>
+                        <p><?php echo $row1["company"]?> </p>
+                        <p><?php echo $row1["description"]?></p>
+                    </div>
+                    <div class="ratings">
+					<?php
+						  $sql="SELECT COUNT(*) FROM `rivew` where id=$id and name='$name'  ";
+						  $conn3 = new mysqli("localhost", "root","","asif");
+						  $result3=$conn->query($sql); 
+						  $row3 = $result3->fetch_assoc();
+						  $count=$row3['COUNT(*)'];
+						  $sql="SELECT * FROM `rivew` where id=$id and name='$name'  ";
+						  $conn3 = new mysqli("localhost", "root","","asif");
+						  $result3=$conn->query($sql); 
+						  if ($result3->num_rows > 0) {$sum=0;
+						  while($row3 = $result3->fetch_assoc()) {
+							  
+							  $sum=$sum+$row3['rating'];
+							  
+						  }$avg=$sum/$count;}
+						  
+						  $conn3->close();?>
+                        <p class="pull-right"><?php echo $count ?> reviews</p>
+                        <p>
+                            <?php $num = round($avg);
+						$num1=5;
+						while($num>=1){?>
+						<span class="glyphicon glyphicon-star"></span><?php $num=$num-1;$num1=$num1-1;}
+                         while($num1>0){?>
+						<span class="glyphicon glyphicon-star-empty"></span><?php $num1=$num1-1;}?>   
+                          <?php echo round($avg); ?> stars
+                        </p>
+                    <div style="height:150px">
+					<?php $flag=$_GET['flag'];
+							if($flag==1){?><p>Product added to cart</p><?php } 
+							if($flag==2){?><p>Product already in cart</p><?php }
+							if($flag==3){?><p>Product out of stock</p><?php }
+							if($flag==4){?><p>Please input quantity</p><?php } ?>
+					<form accept-charset="UTF-8" class="pull-right" action="http://127.0.0.1/asif/php-login/index.php?" method="get" >
+									
+									<input type="text" class="form-control" placeholder="input quantity" name="quantity" />
+									<input type="hidden" name="flag" value="3" />
+									<input type="hidden" name="name" value="<?php echo $name ?>" />
+									<input type="hidden" name="id" value="<?php echo $id ?>" /><br>
+									<button class="btn btn-success btn-lg pull-right"  type="submit"><i class="fa fa-fw fa-shopping-cart"></i>Add to cart</button>
+									
+								</form><br>
+					</div></div>
+                </div>
+
+                <div class="well">
+
+                    <div class="well-sm">
+						<div class="text-right">
+							<?php if($flag==5){?><p>Input rating or comment</p><?php } ?>
+							<a class="btn btn-success btn-green" href="#reviews-anchor" id="open-review-box"><i class="fa fa-fw fa-book"></i>Leave a Review</a>
+						</div>
 					
-
-					<div class="col-md-12">
-						<?php 
-						$sql = "SELECT * FROM `products`,shops where products.shop_id=shops.shop_id and products.shop_id=$id and name='$name'";
-				
-				
-						if ($conn->query($sql)) {
-
-							$result = $conn->query($sql);
-
-						if ($result->num_rows > 0) {
-						while($row = $result->fetch_assoc()) {{?>
-						<h4><?php echo $row["shop_name"]?></p></h4>
-						<div class="thumbnail">
-						
-							<img class="img-responsive" src="getimg1.php?name=<?php echo $row["name"]?>&id=<?php echo $row["shop_id"]?>" alt="">
-							<div class="caption-full">
-								<h4 class="pull-right"><i class="fa fa-fw fa-inr"></i><?php echo $row["price"]?></h4>
-								<h4><?php echo $row["name"]?>
-								</h4>
-								<h4><?php if(!$row["stock"]){echo "out of stock";} ?></h4>
-								<p><?php echo $row["company"]?> </p>
-								<p><?php echo $row["product_details"]?></p>
-							</div>
-							<div class="ratings">
-							<?php
-								  $sql="SELECT COUNT(*) FROM `rivew` where id=$id and name='$name'  ";
-								  
-								  $result3=$conn->query($sql); 
-								  $row3 = $result3->fetch_assoc();
-								  $count=$row3['COUNT(*)'];
-								  $sql="SELECT * FROM `rivew` where id=$id and name='$name'";
-								  $conn3 = new mysqli("localhost", "root","","asif");
-								  $result3=$conn->query($sql); 
-								  if ($result3->num_rows > 0) {$sum=0;
-								  while($row3 = $result3->fetch_assoc()) {
-									  
-									  $sum=$sum+$row3['rating'];
-									  
-								  }$avg=$sum/$count;}
-								  
-								  $conn3->close();
-								 ?>
-								<p class="pull-right" id="dispcount"><?php echo $count ?> reviews</p>
-								<p id="ratstar">
-									<?php $num = round($avg);
-								$num1=5;
-								while($num>=1){?>
-								<span class="glyphicon glyphicon-star"></span><?php $num=$num-1;$num1=$num1-1;}
-								 while($num1>0){?>
-								<span class="glyphicon glyphicon-star-empty"></span><?php $num1=$num1-1;}?>   
-								  <?php echo round($avg); ?> stars
-								</p>
-							<div style="height:110px">
-							
-							<form accept-charset="UTF-8" class="pull-right" id="addcart" method="get" >
-											
-											<input type="text" class="form-control" placeholder="input quantity" name="quantity" />
-											<input type="hidden" name="flag" value="3" />
-											<input type="hidden" name="name" value="<?php echo $name ?>" />
-											<input type="hidden" name="id" value="<?php echo $id ?>" /><br>
-											<button class="btn btn-success pull-right"  type="submit"><i class="fa fa-fw fa-shopping-cart"></i>Add to cart</button>
-											
-										</form><br>
-										<p id="addcartstatus"></p>
-							</div></div>
-						</div>
-
-						<div class="well">
-
-							<div class="well-sm">
-								<div class="text-right">
-									<p id="reviewmsg"></p>
-									<a class="btn btn-success btn-green" href="#reviews-anchor" id="open-review-box"><i class="fa fa-fw fa-book"></i>Leave a Review</a>
-								</div>
-							
-								<div class="row" id="post-review-box" style="display:none;">
-									<div class="col-md-12">
-										<form accept-charset="UTF-8" id="submitreview"  method="post">
-										
-											<input id="ratings-hidden" name="rating" type="hidden"> 
-											<input class="form-control animated" cols="50" id="new-review" name="comment" placeholder="Enter your review here..." type="text" rows="5">
-											<input type="hidden" name="flag" value="1" />
-											<input type="hidden" name="name" value="<?php echo $name ?>" />
-											<input type="hidden" name="id" value="<?php echo $id ?>" />
-											<div class="text-right">
-												<div class="stars starrr" data-rating="0"></div>
-												<a class="btn btn-danger btn-sm" href="#" id="close-review-box" style="display:none; margin-right: 10px;">
-												<span class="glyphicon glyphicon-remove"></span>Cancel</a>
-												<button class="btn btn-success btn-lg" type="submit"><i class="fa fa-fw fa-floppy-o"></i>Save</button>
-											</div>
-										</form>
+						<div class="row" id="post-review-box" style="display:none;">
+							<div class="col-md-12">
+								<form accept-charset="UTF-8" action="http://127.0.0.1/asif/php-login/index.php? method="get">
+								
+									<input id="ratings-hidden" name="rating" type="hidden"> 
+									<textarea class="form-control animated" cols="50" id="new-review" name="comment" placeholder="Enter your review here..." rows="5"></textarea>
+									<input type="hidden" name="flag" value="1" />
+									<input type="hidden" name="name" value="<?php echo $name ?>" />
+									<input type="hidden" name="id" value="<?php echo $id ?>" />
+									<div class="text-right">
+										<div class="stars starrr" data-rating="0"></div>
+										<a class="btn btn-danger btn-sm" href="#" id="close-review-box" style="display:none; margin-right: 10px;">
+										<span class="glyphicon glyphicon-remove"></span>Cancel</a>
+										<button class="btn btn-success btn-lg" type="submit"><i class="fa fa-fw fa-floppy-o"></i>Save</button>
 									</div>
-								</div>
-							</div> 
-
-							<div id="review-box">
-									
-									<?php $sql="SELECT * FROM `rivew` where id=$id and name='$name'  ORDER BY `time` DESC  ";
-										  
-										  $result3=$conn->query($sql); 
-										  
-
-										if ($result3->num_rows > 0) {
-											
-											while($row3 = $result3->fetch_assoc()) {?>
-									<hr>
-									<div class="row" >
-										<div class="col-md-12" >
-										<?php $num = $row3['rating'];
-										$num1=5;
-										while($num>0){?>
-										<span class="glyphicon glyphicon-star"></span><?php $num=$num-1;$num1=$num1-1;}
-										 while($num1>0){?>
-										<span class="glyphicon glyphicon-star-empty"></span><?php $num1=$num1-1;}?>   
-											
-											<?php echo $row3['username'];?>
-											<span class="pull-right"><?php echo date($row3['time']);?></span>
-											<p><?php echo $row3['comment'];?></p>
-										</div>
-									</div>
-
-									<?php }}?>
+								</form>
 							</div>
 						</div>
+					</div> 
 
-					</div>
+                    
+					<?php $sql="SELECT * FROM `rivew` where id=$id and name='$name'  ";
+						  $conn3 = new mysqli("localhost", "root","","asif");
+						  $result3=$conn->query($sql); 
+						  
 
-				
+						if ($result3->num_rows > 0) {
+							
+							while($row3 = $result3->fetch_assoc()) {?>
+					<hr>
+                    <div class="row">
+                        <div class="col-md-12">
+						<?php $num = $row3['rating'];
+						$num1=5;
+						while($num>0){?>
+						<span class="glyphicon glyphicon-star"></span><?php $num=$num-1;$num1=$num1-1;}
+                         while($num1>0){?>
+						<span class="glyphicon glyphicon-star-empty"></span><?php $num1=$num1-1;}?>   
+                            
+                            <?php echo $row3['username'];?>
+                            <span class="pull-right"><?php echo date($row3['time']);?></span>
+                            <p><?php echo $row3['comment'];?></p>
+                        </div>
+                    </div>
 
-			</div>
-									
-			<?php }}
-									} else {
-									echo "0 results";
-									}
-									} else {
-									echo "Error creating table: " . $conn->error;
-									}
+						<?php $conn3->close();}}?>
 
-									$conn->close();
-									$username=$_SESSION['user_name']; 
-									?>
-	
-	
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+							<?php }}
+							} else {
+							echo "0 results";
+							}
+							} else {
+							echo "Error creating table: " . $conn1->error;
+							}
+
+							$conn1->close();
+							?>
+    <!-- /.container -->
+	<?php }}
+							} else {
+							echo "0 results";
+							}
+							} else {
+							echo "Error creating table: " . $conn->error;
+							}
+
+							$conn->close();
+							?>
     <div class="container">
 
         <hr>
 
         <!-- Footer -->
-        <footer class="text-center">
-       
-        <div class="footer-below">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-4">
-                        Copyright &copy; Gokochi! 2016
-                    </div>
-					<div class="col-md-4">
-                    <ul class="list-inline social-buttons">
-                        <li><a href="#"><i class="fa fa-twitter"></i></a>
-                        </li>
-                        <li><a href="#"><i class="fa fa-facebook"></i></a>
-                        </li>
-                        <li><a href="#"><i class="fa fa-linkedin"></i></a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-md-4" >
-                    <ul class="list-inline quicklinks">
-                        <li><a  href="#">Privacy Policy</a>
-                        </li>
-                        <li><a href="#">Terms of Use</a>
-                        </li>
-                    </ul>
-                </div>
-					
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Gokochi! 2016</p>
                 </div>
             </div>
-        </div>
-    </footer>
+        </footer>
 
-    </div></div></div></div>
-	<!-- /.container -->
+    </div>
+    <!-- /.container -->
 
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
-	 <script type="text/javascript">
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
-    </script>
-	 <script type="text/javascript">
-	 $("#addcart").submit(function(e) {
-
-    var url = "addcart.php"; // the script where you handle the form input.
-
-    $.ajax({
-           type: "POST",
-           url: url,
-           data: $("#addcart").serialize(), // serializes the form's elements.
-           success: function change(data)
-          {document.getElementById("addcartstatus").innerHTML = data;
-		   
-			$('#addcart').trigger("reset");}
-			
-         });
-
-    e.preventDefault(); // avoid to execute the actual submit of the form.
-});
-</script>
-<script type="text/javascript">
-        if(parseInt(<?php echo $count; ?>))
-		var cou =parseInt(<?php echo $count; ?>);
-		else 
-		var cou=parseInt(0);
-		if(parseInt(<?php echo $count; ?>))
-		var su = parseInt(<?php echo $sum; ?>);
-		else 
-		var su=parseInt(0);
-	    
-		var avg=parseInt(0);
-	 $("#submitreview").submit(function(e) {
-
-    var url = "insertrivew.php"; // the script where you handle the form input.
-
-    $.ajax({
-           type: "POST",
-           url: url,
-           data: $("#submitreview").serialize(), // serializes the form's elements.
-           success: function change(data)
-          {document.getElementById("reviewmsg").innerHTML = data;
-		   
-			
-			if(data=="review inserted")
-			{
-			var qwe = "<hr><div class=\"row\" ><div class=\"col-md-12\">";
-			
-			var cc = 0;
-			if(parseInt(document.getElementById("ratings-hidden").value))
-				cc=parseInt(document.getElementById("ratings-hidden").value);
-			
-			cou+=1;
-			
-			su=parseInt(su)+parseInt(cc);
-			avg=parseInt(su)/parseInt(cou);
-			avg=Math.round(avg);
-			var vv = document.getElementById("new-review").value;
-			for(var i=0;i<cc;++i)
-				qwe += "<span class=\"glyphicon glyphicon-star\" style=\"padding-right: 4px;\"></span>";
-			for(var i=cc;i<5;++i)
-				qwe += "<span class=\"glyphicon glyphicon-star-empty\" style=\"padding-right: 4px;\"></span>";
-			
-			qwe += "<?php echo $username; ?> <span class=\"pull-right\">Now</span><p>";
-			qwe += vv;
-			qwe += "</p></div></div>"
-			
-			$("#review-box").prepend(qwe);
-			document.getElementById("dispcount").innerHTML = cou +' reviews';
-			var red="";
-			for(var i=0;i<avg;++i)
-				red += "<span class=\"glyphicon glyphicon-star\" style=\"padding-right: 4px;\"></span>";
-			for(;i<5;++i)
-				red += "<span class=\"glyphicon glyphicon-star-empty\" style=\"padding-right: 4px;\"></span>";
-			    red += avg;
-				red +=" stars</p>";
-				document.getElementById("ratstar").innerHTML = red;
-			//$("#ratstar").replaceWith(red);
-			$('#submitreview').trigger("reset");
-		  }}
-			
-         });
-
-    e.preventDefault(); // avoid to execute the actual submit of the form.
-});
-</script>
 
 </body>
 
