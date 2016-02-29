6ec78f137b42b4d1241e724cc8bf0c831633ef6b
asif abdul rahiman <asif abdul rahiman>
2016-02-29 20:20:43 +0530

    asdf

--- php-login/index.php
+++ php-login/index.php
@@ -30,7 +30,7 @@ require_once('libraries/PHPMailer.php');
 require_once('classes/Login.php');
 
 // create a login object. when this object is created, it will do all login/logout stuff automatically
-// so this single line handles the entire login process.
+
 $login = new Login();
 
 // ... ask if we are logged in here:
